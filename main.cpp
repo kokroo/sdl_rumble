523a81bc46951595e781f5bf306682352bc0bc65
Shiv Kokroo <kokroo@users.noreply.github.com>
2024-11-12 18:07:17 +0530

    Apple Xbox 360 Virtual Rumble working

--- main.cpp
+++ main.cpp
@@ -1,44 +1,91 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-
 int main(void)
 {
-	SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI_PS4_RUMBLE, "1");
-	SDL_Init(SDL_INIT_JOYSTICK);
+    // Initialize SDL with video and joystick
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
 
-	printf("testing rumble for connected controllers\n");
+    // Create an SDL window (required for macOS gamepad detection)
+    SDL_Window* window = SDL_CreateWindow("SDL Gamepad Test",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
 
-	SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt");
-	printf("joysticks: %i\n\n", SDL_NumJoysticks());
+    if (!window)
+    {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
 
-	for(int i=0; i < SDL_NumJoysticks(); i++ )
-	{
-		SDL_GameController* gGameController = SDL_GameControllerOpen( i );
+    SDL_SetHint(SDL_HINT_JOYSTICK_MFI, "1");
+    SDL_SetHint(SDL_HINT_JOYSTICK_IOKIT, "1");
 
-		if (gGameController != NULL)
-		{
-			printf("controller %i: %s\n", i, SDL_GameControllerName( gGameController ));
+    printf("Waiting for controllers...\n");
 
-			if (SDL_GameControllerHasRumble( gGameController ))
-			{
-				SDL_GameControllerRumble( gGameController, 0xFFFF * 3 / 4, 0xFFFF * 3 / 4, 500 );
-				SDL_Delay(500);
-				SDL_GameControllerRumble( gGameController, 0, 0, 0 );
-				printf("rumbled!\n\n");
-			}
-			else
-			{
-				printf("rumble not supported\n\n");
-			}
-		}
-		else
-		{
-			printf("no controller connected: %s\n", SDL_GetError() );
-		}
-	}
+    // Load custom controller mappings (if any)
+    SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt");
 
-	printf("all done, bye!\n");
-	SDL_Quit();
+    // Event loop
+    SDL_Event e;
+    int running = 1;
+    while (running)
+    {
+        // Process events
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                running = 0;
+            }
+        }
+
+        // Check for connected controllers
+        int numJoysticks = SDL_NumJoysticks();
+        printf("Connected controllers: %d\n", numJoysticks);
+
+        for (int i = 0; i < numJoysticks; i++)
+        {
+            SDL_GameController* gGameController = SDL_GameControllerOpen(i);
+            if (gGameController != NULL)
+            {
+                printf("Controller %i: %s, address: %p\n", i, SDL_GameControllerName(gGameController), (void*)gGameController);
+
+                // Check for rumble capability and initiate rumble
+                if (SDL_GameControllerHasRumble(gGameController))
+                {
+                    SDL_GameControllerRumble(gGameController, 0xFFFF * 3 / 4, 0xFFFF * 3 / 4, 500);
+                    SDL_Delay(500); // Optional delay to show rumble effect
+                    SDL_GameControllerRumble(gGameController, 0, 0, 0);
+                    printf("Rumble on controller %d\n\n", i);
+                }
+                else
+                {
+                    printf("Rumble not supported on controller %d\n\n", i);
+                }
+
+                SDL_GameControllerClose(gGameController);
+            }
+            else
+            {
+                printf("No controller connected: %s\n", SDL_GetError());
+            }
+        }
+
+        SDL_Delay(1000); // Delay for periodic checking
+    }
+
+    printf("Exiting program...\n");
+
+    // Clean up and quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
-
